ab9dccbfbf41e41ff4e0690d025efe23a88e7471
PBelugin <pashaigood@gmail.com>
2014-07-06 17:18:45 +0400

    https://pintask.me/board/RrWPcysqhyQgHTnLE/card/sdHbXQEYzJMbjFMpv Логика не тестовых данных, тестовый вид. Готово к переходу на реальные данные.

--- app/scripts/datas/proposal_list.js
+++ app/scripts/datas/proposal_list.js
@@ -21,15 +21,21 @@ module.exports = [
     {
         route: [
             {
-                name: 'Минск',
-                date: '12.07.14',
+                name: 'Мурманск',
+                date: '15.07.14',
                 event: ''
             },
 
             {
-                name: 'Прага',
-                date: '15.07.14',
-                event: 'PrahaCon'
+                name: 'Владивосток',
+                date: '16.07.14',
+                event: 'УходимКон'
+            },
+
+            {
+                name: 'Москва',
+                date: '26.07.14',
+                event: 'МосковКон'
             }
         ],
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis',
@@ -53,5 +59,43 @@ module.exports = [
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis',
         users: 4,
         comments: 4
+    },
+
+    {
+        route: [
+            {
+                name: 'Минск',
+                date: '12.07.14',
+                event: ''
+            },
+
+            {
+                name: 'Прага',
+                date: '15.07.14',
+                event: 'PrahaCon'
+            }
+        ],
+        description: 'Lorem ipsum dolor sit amet',
+        users: 12,
+        comments: 8
+    },
+
+    {
+        route: [
+            {
+                name: 'Минск',
+                date: '12.07.14',
+                event: ''
+            },
+
+            {
+                name: 'Прага',
+                date: '15.07.14',
+                event: 'PrahaCon'
+            }
+        ],
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit',
+        users: 2,
+        comments: 8
     }
 ];
